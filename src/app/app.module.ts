6e9cfc09a2a92044f583079ed3ff5e839f051cdc
Sapna OSP <sapna@osplabs.com>
2023-05-30 20:11:10 +0530

    first all code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNestedTreeNode, MatTreeModule } from '@angular/material/tree';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -10,9 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatNestedTreeNode,
+    MatIconModule,
+    NgModule 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
